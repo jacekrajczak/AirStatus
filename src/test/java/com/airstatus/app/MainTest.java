4b0f65e39380344c8147a57e6e2bbd87fa08bd25
Jacek Rajczak <jacek.rajczak123@gmail.com>
2019-05-05 11:16:53 +0200

    init schema

---
+++ src/test/java/com/airstatus/app/MainTest.java
@@ -0,0 +1,18 @@
+package com.airstatus.app;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple MainTest.
+ */
+public class MainTest
+{
+
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
